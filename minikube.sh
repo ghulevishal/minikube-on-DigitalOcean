98e6d4df38658f3a8af65ae1c8182f83b6cb898a
Sumit Murari <sumit@cloudyuga.guru>
2019-01-16 14:50:43 +0530

    iP fix

--- minikube.sh
+++ minikube.sh
@@ -2,14 +2,12 @@
 
 name=$1
 sshkey=$2
-source ~/.bashrc
 
 doctl compute droplet create --size 4gb --image ubuntu-16-04-x64 --region blr1 --ssh-keys $sshkey --enable-private-networking --tag-name $name $name
 
 sleep 30s
 
-IP="$(doctl compute droplet list | awk '/$name/ {print $3}')"
-
+IP="$(doctl compute droplet list ${name} --no-header --format PublicIPv4)"
 
 
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null install.sh root@$IP:~/.
