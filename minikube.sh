b1fc2d56784b60413f886028ec09b471da5410af
vishalcloudyuga <vishal@cloudyuga.guru>
2019-01-17 14:29:35 +0530

    update

--- minikube.sh
+++ minikube.sh
@@ -1,19 +1,28 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 name=$1
 sshkey=$2
 
 doctl compute droplet create --size 4gb --image ubuntu-16-04-x64 --region blr1 --ssh-keys $sshkey --enable-private-networking --tag-name $name $name
 
-sleep 30s
+while true; do
+   status=$(doctl compute droplet ls ${name} --no-header --format "Status")
+   if [ "$status" == "active" ]; then
+      echo "Droplet is active"
+      break
+   fi
+   echo "Waiting for 2 sec. Dropet not ready yet. Status=$status"
+   sleep 2
+done
 
 IP="$(doctl compute droplet list ${name} --no-header --format PublicIPv4)"
-
 
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null install.sh root@$IP:~/.
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP 'bash install.sh'
 
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP:~/.kube/config kubeconfig
 
-
+echo "Your Minikube IP is $IP" >ip.txt
+cat ip.txt
 
