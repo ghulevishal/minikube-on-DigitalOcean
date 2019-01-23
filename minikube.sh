51e0e42366c7040e7405c3cc06860194a2e4a54a
Sumit Murari <sumit@cloudyuga.guru>
2019-01-23 11:58:56 +0530

    Hack to avoid ssh connect timeout.
    Added loop to wait till ssh works

--- minikube.sh
+++ minikube.sh
@@ -19,12 +19,13 @@ if [ -z $sshkey ]; then
     usage
 fi
 
-doctl compute droplet create --size 4gb --image ubuntu-16-04-x64 --region blr1 --ssh-keys $sshkey --enable-private-networking --tag-name $name $name
+doctl compute droplet create --size 8gb --image ubuntu-16-04-x64 --region blr1 --ssh-keys $sshkey --enable-private-networking --tag-name $name $name
 
 while true; do
    status=$(doctl compute droplet ls ${name} --no-header --format "Status")
    if [ "$status" == "active" ]; then
       echo "Droplet is active"
+      sleep 2
       break
    fi
    echo "Waiting for 2 sec. Dropet not ready yet. Status=$status"
@@ -33,12 +34,15 @@ done
 
 IP="$(doctl compute droplet list ${name} --no-header --format PublicIPv4)"
 
+
+while ! ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP hostname; do
+    echo "Waiting for ssh to be active"
+    sleep 1
+done
+
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null install.sh root@$IP:~/.
-
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP 'bash install.sh'
-
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP:~/.kube/config kubeconfig
 
 echo "Your Minikube IP is $IP" >ip.txt
 cat ip.txt
-
