763625cbf2714ea580a8206385b9582f8d1c7bb3
Vishal Ghule <vghule9994@gmail.com>
2022-01-24 11:51:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ apt-get install -y kubectl socat conntrack
 }
 
 {
+###Installing the Minikube
 
   curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
 
